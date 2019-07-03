e4ca682614bc9f0c4b56893af58495cd29dc3eda
RyotaMoriyama <info@ryotamoriyama.com>
2019-07-04 03:52:31 +0900

    Modify import functions

--- functions/02_import_functions.php
+++ functions/02_import_functions.php
@@ -1,11 +1,6 @@
 <?php
-
 /**
- * get_template_part の変わりに利用してください。
- * $args をローカルスコープにて渡すことが可能です。
- *
- * @param       $tpl
- * @param array $vars
+ * Import templates
  */
 function import_template($tpl, $vars = array())
 {
@@ -18,30 +13,12 @@ function import_template($tpl, $vars = array())
     include $path;
 }
 
-// 後方互換性のための代理名
-function importTemplate($tpl, $vars = array())
-{
-    trigger_error('The function importTemplate was renamed to import_template.', E_USER_DEPRECATED);
-
-    return call_user_func_array('import_template', func_get_args());
-}
-
-/**
- * parts/ 以下のテンプレートを読み込むのに利用してください。
- * $args をローカルスコープにて渡すことが可能です。
- *
- * @param       $tpl
- * @param array $vars
- */
 function import_part($tpl, $vars = array())
 {
     import_template('parts/'.ltrim($tpl, '/'), $vars);
 }
 
-// 後方互換性のための代理名
-function importPart($tpl, $vars = array())
+function import_modulet($tpl, $vars = array())
 {
-    trigger_error('The function importPart was renamed to import_part.', E_USER_DEPRECATED);
-
-    return call_user_func_array('import_part', func_get_args());
+    import_template('modules/'.ltrim($tpl, '/'), $vars);
 }
