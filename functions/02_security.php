3ca307d1b41197335c6689990213e5e127c44b90
RyotaMoriyama <info@ryotamoriyama.com>
2019-07-04 15:21:50 +0900

    Modify

--- functions/02_security.php
+++ functions/02_security.php
@@ -13,13 +13,13 @@ define('AUTOMATIC_UPDATER_DISABLED', true);
 /**
  * Disable xmlrpc.php
  */
-add_filter(‘xmlrpc_enabled’, ‘__return_false’);
+add_filter('xmlrpc_enabled', '__return_false');
 
 function remove_x_pingback($headers) {
-    unset($headers[‘X-Pingback’]);
+    unset($headers['X-Pingback']);
     return $headers;
 }
-add_filter(‘wp_headers’, ‘remove_x_pingback’);
+add_filter('wp_headers', 'remove_x_pingback');
 
 
 /**
