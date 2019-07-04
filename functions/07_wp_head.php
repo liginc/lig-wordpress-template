4d7806f97d30b8967a916d2eeec6649b0a29be88
RyotaMoriyama <info@ryotamoriyama.com>
2019-07-04 15:27:18 +0900

    Add helper functions for wp_head

---
+++ functions/07_wp_head.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * disable emoji
+ */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_shortlink_wp_head');
