ad8942e495fe9eb3bf51f5582523012ccb11d110
RyotaMoriyama <info@ryotamoriyama.com>
2019-12-12 19:12:48 +0900

    Replace functions on header and footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php wp_footer(); ?>
 
-<script src="<?php echo resolve_asset_url('/js/app.js'); ?>" async></script>
+<script src="<?php echo resolve_uri('/js/app.js'); ?>" async></script>
 
 </body>
 </html>
