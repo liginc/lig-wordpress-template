8629e56e050aff6e07ed62bfc120966341afe5ee
RyotaMoriyama <info@ryotamoriyama.com>
2019-12-12 21:02:07 +0900

    Modify

--- functions/20_disable_comment.php
+++ functions/20_disable_comment.php
@@ -28,3 +28,13 @@ function hide_comment_menus()
 {
     remove_menu_page('edit-comments.php');
 }
+
+/**
+ * 記事一覧からコメントカラム削除
+ */
+add_filter('manage_posts_columns', 'customize_admin_manage_posts_comment_column');
+function customize_admin_manage_posts_comment_column($columns)
+{
+    unset($columns['comments']); //コメントカラムの削除
+    return $columns;
+}
