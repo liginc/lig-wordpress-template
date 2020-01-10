a316c411da65045ab73b6d42d342d5675452e335
RyotaMoriyama <info@ryotamoriyama.com>
2020-01-10 18:55:18 +0900

    Change glob dir

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 /*
  * functionsフォルダにあるファイルをすべて読み込む
 */
-foreach (glob(TEMPLATEPATH . '/functions/*.php') as $file) {
+foreach (glob(TEMPLATEPATH . '/functions/autoload/*.php') as $file) {
     require_once $file;
 }
