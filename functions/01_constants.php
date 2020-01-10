2786adba2c4c61672ba6ac0181267748aec8b4ea
RyotaMoriyama <info@ryotamoriyama.com>
2020-01-10 18:09:22 +0900

    Modify constants

--- functions/01_constants.php
+++ functions/01_constants.php
@@ -1,50 +1,55 @@
 <?php
 
-/**
- * General Setting
- */
-define('HOME_URL', home_url() . '/');
-define('SITE_NAME', get_bloginfo('name'));
+add_action('init', 'set_up_constants');
 
-/**
- * ASSETS
- */
-define('ASSETS', 'assets/');
-define('IMAGES', ASSETS . 'images/');
-define('JS', ASSETS . 'js/');
-define('CSS', ASSETS . 'css/');
-define('SVG', ASSETS . 'svg/');
+function set_up_constants()
+{
+    /**
+     * General Setting
+     */
+    define('HOME_URL', home_url() . '/');
+    define('SITE_NAME', get_bloginfo('name'));
 
-define('FAVICON', resolve_uri(IMAGES . 'favicon.ico'));
-define('TOUCH_ICON', resolve_uri(IMAGES . 'apple-touch-icon-precomposed.png'));
-define('APP_JS', resolve_uri(JS . 'app.js'));
-define('APP_CSS', resolve_uri(CSS . 'app.css'));
-define('NO_IMAGE', resolve_uri(IMAGES . 'noimage.png'));
-define('OGP_IMAGE', resolve_uri(IMAGES . 'ogp.png'));
+    /**
+     * ASSETS
+     */
+    define('URL_ASSETS', 'assets/');
+    define('URL_IMAGES', URL_ASSETS . 'images/');
+    define('URL_JS', URL_ASSETS . 'js/');
+    define('URL_CSS', URL_ASSETS . 'css/');
+    define('URL_SVG', URL_ASSETS . 'svg/');
 
-/**
- * CONTENTS
- */
-/*
-define('ABOUT', HOME_URL . 'about/');
-define('SERVICE', HOME_URL . 'service/');
-define('CONTACT', HOME_URL . 'contact/');
-define('CONFIRM', CONTACT . 'confirm/');
-define('COMPLETE', CONTACT . 'complete/');
-*/
+    define('URL_FAVICON', resolve_uri(URL_IMAGES . 'favicon.ico'));
+    define('URL_TOUCH_ICON', resolve_uri(URL_IMAGES . 'apple-touch-icon-precomposed.png'));
+    define('URL_APP_JS', resolve_uri(URL_JS . 'app.js'));
+    define('URL_APP_CSS', resolve_uri(URL_CSS . 'app.css'));
+    define('URL_NO_IMAGE', resolve_uri(URL_IMAGES . 'noimage.png'));
+    define('URL_OGP_IMAGE', resolve_uri(URL_IMAGES . 'ogp.png'));
 
-/**
- * SNS
- */
-/*
-define('TWITTER', 'https://twitter.com/');
-define('FACEBOOK', 'https://facebook.com/');
-define('INSTAGRAM', 'https://instagram.com/');
-*/
+    /**
+     * CONTENTS
+     */
+    /*
+    define('URL_ABOUT', HOME_URL . 'about/');
+    define('URL_SERVICE', HOME_URL . 'service/');
+    define('URL_CONTACT', HOME_URL . 'contact/');
+    define('URL_CONFIRM', CONTACT . 'confirm/');
+    define('URL_COMPLETE', CONTACT . 'complete/');
+    */
 
-/**
- * EXTERNAL LINKS
- */
-/*
-define('', '');
-*/
+    /**
+     * SNS
+     */
+    /*
+    define('URL_TWITTER', 'https://twitter.com/');
+    define('URL_FACEBOOK', 'https://facebook.com/');
+    define('URL_INSTAGRAM', 'https://instagram.com/');
+    */
+
+    /**
+     * EXTERNAL LINKS
+     */
+    /*
+    define('', '');
+    */
+}
