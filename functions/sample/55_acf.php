70bfb131d45592dd9e7dc40a42ef447f568bed94
RyotaMoriyama <info@ryotamoriyama.com>
2020-05-21 15:06:25 +0900

    Add acf helper functions

--- functions/sample/55_acf.php
+++ functions/sample/55_acf.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ryota
- * Date: 2019/07/04
- * Time: 4:57
- */
+
+if (defined('ACF')) {
+
+    //add_action('acf/init', 'display_acf_option_page');
+    function display_acf_option_page()
+    {
+
+        if (function_exists('acf_add_options_page')) {
+            acf_add_options_page(
+                [
+                    'page_title' => 'サイト設定',
+                    'menu_title' => 'サイト設定',
+                    'menu_slug' => 'acf-options'
+                ]
+            );
+        }
+    }
+
+    //add_action('acf/init', 'display_acf_option_sub_page');
+    function display_acf_option_sub_page()
+    {
+        if (function_exists('acf_add_options_sub_page')) {
+            acf_add_options_sub_page(
+                [
+                    'page_title' => 'サブページ',
+                    'menu_title' => 'サブページ',
+                    'parent_slug' => 'acf-options',
+                ]
+            );
+        }
+    }
+
+}
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
     <link rel="shortcut icon" href="<?= URL_FAVICON ?>">
     <link rel="apple-touch-icon-precomposed" href="<?= URL_TOUCH_ICON ?>">
