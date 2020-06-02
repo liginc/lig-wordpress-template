fb6cf6ad953eac2bfe0e119c08c0bed2e59973fb
RyotaMoriyama <info@ryotamoriyama.com>
2020-06-02 12:19:35 +0900

    Modify admin_bar_menu

--- functions/autoload/25_admin_bar_menus.php
+++ functions/autoload/25_admin_bar_menus.php
@@ -3,17 +3,13 @@
 function remove_bar_menus( $wp_admin_bar ) {
     $wp_admin_bar->remove_menu( 'wp-logo' );      // ロゴ
 //    $wp_admin_bar->remove_menu( 'site-name' );    // サイト名
-//    $wp_admin_bar->remove_menu( 'view-site' );    // サイト名 -> サイトを表示
-//    $wp_admin_bar->remove_menu( 'dashboard' );    // サイト名 -> ダッシュボード (公開側)
-//    $wp_admin_bar->remove_menu( 'themes' );       // サイト名 -> テーマ (公開側)
     $wp_admin_bar->remove_menu( 'customize' );    // サイト名 -> カスタマイズ (公開側)
     $wp_admin_bar->remove_menu( 'comments' );     // コメント
     $wp_admin_bar->remove_menu( 'updates' );      // 更新
     $wp_admin_bar->remove_menu( 'view' );         // 投稿を表示
-    $wp_admin_bar->remove_menu( 'new-content' );  // 新規
-    $wp_admin_bar->remove_menu( 'new-post' );     // 新規 -> 投稿
+//    $wp_admin_bar->remove_menu( 'new-content' );  // 新規
+//    $wp_admin_bar->remove_menu( 'new-post' );     // 新規 -> 投稿
     $wp_admin_bar->remove_menu( 'new-media' );    // 新規 -> メディア
-    $wp_admin_bar->remove_menu( 'new-link' );     // 新規 -> リンク
     $wp_admin_bar->remove_menu( 'new-page' );     // 新規 -> 固定ページ
     $wp_admin_bar->remove_menu( 'new-user' );     // 新規 -> ユーザー
 //    $wp_admin_bar->remove_menu( 'my-account' );   // マイアカウント
