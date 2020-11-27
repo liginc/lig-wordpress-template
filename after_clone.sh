94fff4780ce9a20cde19fb1f2a2eb5c8e32688d3
RyotaMoriyama <info@ryotamoriyama.com>
2020-11-27 19:49:54 +0900

    Add sample code

--- after_clone.sh
+++ after_clone.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 cd $1
 npm i lazyload reset-css sanitize.css smoothscroll-polyfill viewport-extra
+#SPLIT_PATH=(${1//\// })
+#THEME=${SPLIT_PATH[@]: -1}
+#mv wp/wp-content/themes/${THEME}/functions/extra/.php wp/wp-content/themes/${THEME}/functions/autoload/.php
