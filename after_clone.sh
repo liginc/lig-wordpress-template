95c28574686e75094e0eae9d2370e73f11d8d5c7
RyotaMoriyama <info@ryotamoriyama.com>
2021-01-28 11:26:42 +0900

    Add import-glob-loader

--- after_clone.sh
+++ after_clone.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-cd $1
-npm i import-glob-loader lazyload reset-css sanitize.css smoothscroll-polyfill viewport-extra
-rm -Rf node_modules
+#cd $1
+#npm i import-glob-loader lazyload reset-css sanitize.css smoothscroll-polyfill viewport-extra
+sed -i.bak "s|.sass(|.webpackConfig({\n        module: {\n            rules: [{\n                test: /\\\.scss/,\n                enforce: 'pre',\n                loader: 'import-glob-loader'\n            }]\n        }\n    })\n    .sass(|g" webpack.mix.js
+rm -f webpack.mix.js.bak
+#rm -Rf node_modules
