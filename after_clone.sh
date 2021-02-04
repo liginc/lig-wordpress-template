4431e97df6bd6e5a3d54d3dd0bbe6d25aefe54f3
RyotaMoriyama <info@ryotamoriyama.com>
2021-02-04 11:17:19 +0900

    Remmove comment out

--- after_clone.sh
+++ after_clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#cd $1
-#npm i import-glob-loader lazyload reset-css sanitize.css smoothscroll-polyfill viewport-extra
+cd $1
+npm i import-glob-loader reset-css sanitize.css smoothscroll-polyfill viewport-extra
 sed -i.bak "s|.sass(|.webpackConfig({\n        module: {\n            rules: [{\n                test: /\\\.scss/,\n                enforce: 'pre',\n                loader: 'import-glob-loader'\n            }]\n        }\n    })\n    .sass(|g" webpack.mix.js
 rm -f webpack.mix.js.bak
-#rm -Rf node_modules
+rm -Rf node_modules
--- resources/themes/lig-wordpress-template/assets/js/app.js
+++ resources/themes/lig-wordpress-template/assets/js/app.js
@@ -1,14 +1,10 @@
-import './module/viewport'
+// vewport-extraを有効にする場合はコメントアウト消してね
+// import './module/viewport'
 
-import LazyLoad from 'lazyload'
 import Menu from './module/menu'
 import SmoothScroll from './module/smooth-scroll'
 
 window.addEventListener('load', function () {
-    //Lazyload
-    if (!('loading' in HTMLImageElement.prototype)) {
-        new LazyLoad(document.querySelectorAll('img[loading="lazy"]'))
-    }
 
     //smooth scroll
     new SmoothScroll().clickEvent()
