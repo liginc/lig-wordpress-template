c4659b948ac9eb91c3ecb0e2016484d23da0be28
RyotaMoriyama <info@ryotamoriyama.com>
2021-02-04 10:56:12 +0900

    Modify js

--- resources/themes/lig-wordpress-template/assets/js/app.js
+++ resources/themes/lig-wordpress-template/assets/js/app.js
@@ -1,24 +1,8 @@
-import LazyLoad from 'lazyload';
-import ViewportExtra from 'viewport-extra';
-import Menu from './module/menu';
-import SmoothScroll from './module/smooth-scroll';
+import './module/viewport'
 
-new ViewportExtra(375)
-
-(function () {
-    var ua = navigator.userAgent
-
-    var sp = ua.indexOf('iPhone') > -1 ||
-        (ua.indexOf('Android') > -1 && ua.indexOf('Mobile') > -1)
-
-    var tab = !sp && (
-        ua.indexOf('iPad') > -1 ||
-        (ua.indexOf('Macintosh') > -1 && 'ontouchend' in document) ||
-        ua.indexOf('Android') > -1
-    )
-
-    if (tab) new ViewportExtra(1024)
-})()
+import LazyLoad from 'lazyload'
+import Menu from './module/menu'
+import SmoothScroll from './module/smooth-scroll'
 
 window.addEventListener('load', function () {
     //Lazyload
@@ -31,5 +15,4 @@ window.addEventListener('load', function () {
 
     //menu
     new Menu()
-
 });
--- resources/themes/lig-wordpress-template/assets/js/module/menu.js
+++ resources/themes/lig-wordpress-template/assets/js/module/menu.js
@@ -1,15 +1,15 @@
-const $burger = document.getElementById('burger');
+const $hamburger = document.getElementById('hamburger');
 const $headerMenu = document.getElementById('header-menu');
 
 export default class Menu {
     constructor() {
-        $burger.addEventListener('click',(e)=>{
+        $hamburger.addEventListener('click',(e)=>{
             if ($headerMenu.classList.contains('is-open')) {
                 $headerMenu.classList.remove('is-open');
-                $burger.classList.remove('is-open');
+                $hamburger.classList.remove('is-open');
             } else {
                 $headerMenu.classList.add('is-open');
-                $burger.classList.add('is-open');
+                $hamburger.classList.add('is-open');
             }
             e.preventDefault();
         });
