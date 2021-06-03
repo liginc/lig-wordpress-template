11758b22a412f956e6de5664a3c93715b7cf92e2
森山良太 <moriyama_ryota@liginc.co.jp>
2021-06-03 14:17:11 +0900

    Split required package install function

--- after_clone.sh
+++ after_clone.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 cd $1
-npm i import-glob-loader reset-css sanitize.css smoothscroll-polyfill viewport-extra
 LF=$'\\\x0A'
 sed -i.bak "s|.sass(|.webpackConfig({${LF}        module: {${LF}            rules: [{${LF}                test: /\\\.scss/,${LF}                enforce: 'pre',${LF}                loader: 'import-glob-loader'${LF}            }]${LF}        }${LF}    })${LF}   .sass(|g" webpack.mix.js
 rm -f webpack.mix.js.bak
-rm -Rf node_modules
