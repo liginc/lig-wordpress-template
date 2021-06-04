e0ccec815d05b787ca1fc2e6ceb48b3dde2f32bb
森山良太 <moriyama_ryota@liginc.co.jp>
2021-06-04 10:46:17 +0900

    Modify package

--- resources/themes/lig-wordpress-template/assets/js/app.js
+++ resources/themes/lig-wordpress-template/assets/js/app.js
@@ -1,10 +1,13 @@
-// vewport-extraを有効にする場合はコメントアウトを削除
+/**
+ * vewport-extraを有効にする場合はインストールしてコメントアウトを削除
+ * npm i viewport-extra
+ */
+
 // import './module/viewport'
 
 import Menu from './module/menu'
 
 window.addEventListener('load', function () {
-
     //menu
     new Menu()
 });
