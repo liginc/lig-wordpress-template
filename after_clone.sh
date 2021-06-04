1fbc662008d12d54b954bc0e1bd2aca410e56a18
森山良太 <moriyama_ryota@liginc.co.jp>
2021-06-04 09:02:26 +0900

    Modify package

--- after_clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd $1
-LF=$'\\\x0A'
-sed -i.bak "s|.sass(|.webpackConfig({${LF}        module: {${LF}            rules: [{${LF}                test: /\\\.scss/,${LF}                enforce: 'pre',${LF}                loader: 'import-glob-loader'${LF}            }]${LF}        }${LF}    })${LF}   .sass(|g" webpack.mix.js
-rm -f webpack.mix.js.bak
